209b26fed2b4a3ba740b4f2569abb39324854416
abradyGDIT <arthur.brady@gdit.com>
2025-04-09 13:44:24 -0400

    Updating logger wrappers

--- cdapython/application_utilities.py
+++ cdapython/application_utilities.py
@@ -95,7 +95,7 @@ def get_valid_log_levels():
     Returns:
         set of module-defined integer codes and strings: all valid labels for log levels that can be passed to Handler.setLevel() (via set_log_level()).
     """
-    return { logger.DEBUG, "DEBUG", logger.INFO, "INFO", logger.WARNING, "WARNING", logger.ERROR, "ERROR", logger.CRITICAL, "CRITICAL" }
+    return { logging.DEBUG, "DEBUG", logging.INFO, "INFO", logging.WARNING, "WARNING", logging.ERROR, "ERROR", logging.CRITICAL, "CRITICAL" }
 
 
 
