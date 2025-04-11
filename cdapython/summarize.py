a337d24a63c91c815ac7554826f387bfce3eef82
abradyGDIT <arthur.brady@gdit.com>
2025-04-11 16:16:46 -0400

    Phase 1 function reorg: eliminated link_to_table for summarize(); output debugging

--- cdapython/summarize.py
+++ cdapython/summarize.py
@@ -1521,7 +1521,7 @@ def summarize(
 
         for result_column in result_dataframe.columns:
             
-            if isinstance( result_dataframe[result_column], list ) and isinstance( result_dataframe[result_column][0], dict ) and 'median' in result_dataframe[result_column][0]:
+            if result_dataframe[result_column].dtype == 'object' and isinstance( result_dataframe[result_column][0], dict ) and 'median' in result_dataframe[result_column][0]:
                 
                 # These are one-element arrays, with the element being a key/value dictionary containing summary stats.
 
