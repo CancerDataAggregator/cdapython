7b1098b679be7ce757b8776c9b8f16c511776e26
abradyGDIT <arthur.brady@gdit.com>
2025-04-11 10:37:00 -0400

    Removing dedicated pypi_package subdir to unify code storage in anticipation of upcoming poetry setup for package management

--- pypi_package/README.md
+++
@@ -1,3 +0,0 @@
-This repo contains all the code currently used for NCI CRDC CDA's `cdapython` front-end Python interface.
-
-
--- pypi_package/src/cdapython/__init__.py
+++
@@ -1,2 +0,0 @@
-from cdapython.explore import column_values, columns, summary_counts, tables
-from cdapython.fetch import fetch_rows
