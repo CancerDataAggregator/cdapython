273ab0dacdb8b8724c5886e49a920d61bfa4cd0e
abradyGDIT <arthur.brady@gdit.com>
2025-04-11 17:45:54 -0400

    Phase 1 function reorg: stabilized summarize functions, restored output-format options; still need to check some sub-functions like match_from_file

--- cdapython/__init__.py
+++ cdapython/__init__.py
@@ -1,6 +1,6 @@
 from cdapython.discover import column_values, columns, tables
 from cdapython.summarize import summarize_files, summarize_subjects, summarize
-from cdapython.get_data import get_file_data, get_subject_data
+from cdapython.get_data import get_file_data, get_subject_data, get_data
 
 from cdapython.logging_wrappers import disable_console_logging, disable_file_logging, enable_console_logging, enable_file_logging, set_log_level
 
