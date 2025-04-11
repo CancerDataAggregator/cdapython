23c60d37fa3af400261e5431d15833eac47615e7
abradyGDIT <arthur.brady@gdit.com>
2025-04-11 15:16:19 -0400

    Phase 1 function reorg: eliminated link_to_table for summarize(); output debugging

--- cdapython/__init__.py
+++ cdapython/__init__.py
@@ -1,5 +1,5 @@
 from cdapython.discover import column_values, columns, tables
-from cdapython.summarize import summarize_files, summarize_subjects
+from cdapython.summarize import summarize_files, summarize_subjects, summarize
 from cdapython.get_data import get_file_data, get_subject_data
 
 from cdapython.logging_wrappers import disable_console_logging, disable_file_logging, enable_console_logging, enable_file_logging, set_log_level
