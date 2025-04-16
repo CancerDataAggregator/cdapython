ab8c5acca5bc0cb2d78d22d4d703296a80eb8ce7
abradyGDIT <arthur.brady@gdit.com>
2025-04-16 15:51:15 -0400

    Updating get_data() code to handle current field sets and updated interface specs

--- cdapython/get_data.py
+++ cdapython/get_data.py
@@ -582,6 +582,7 @@ def get_data(
 
                     else:
                         
+                        print( result_dataframe[column].dtype )
                         result_dataframe[column] = pd.to_numeric( result_dataframe[column] ).round().astype( 'Int64' )
 
                 elif column_data_types[column] in { 'text', 'boolean' }:
