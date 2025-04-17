26a6e1c5081178b5cbe9ada10cc5258a1c74cf0b
abradyGDIT <arthur.brady@gdit.com>
2025-04-17 17:26:10 -0400

    Centralizing and fortifying parameter validation

--- cdapython/get_data.py
+++ cdapython/get_data.py
@@ -432,24 +432,24 @@ def get_data(
 
     print( add_columns )
 
-    try:
-        validate_parameter_values(
-            called_function='get_data',
-            cached_column_metadata=cached_column_metadata,
-            valid_data_sources=valid_data_sources,
-            table=table,
-            match_from_file=match_from_file,
-            data_source=data_source,
-            add_columns=add_columns,
-            exclude_columns=exclude_columns,
-            provenance=provenance,
-            return_data_as=return_data_as,
-            output_file=output_file,
-            log=log
-        )
-    except Exception as e:
-        log.error( e )
-        return
+#    try:
+#        validate_parameter_values(
+#            called_function='get_data',
+#            cached_column_metadata=cached_column_metadata,
+#            valid_data_sources=valid_data_sources,
+#            table=table,
+#            match_from_file=match_from_file,
+#            data_source=data_source,
+#            add_columns=add_columns,
+#            exclude_columns=exclude_columns,
+#            provenance=provenance,
+#            return_data_as=return_data_as,
+#            output_file=output_file,
+#            log=log
+#        )
+#    except Exception as e:
+#        log.error( e )
+#        return
 
     #############################################################################################################################
     # Preprocess table metadata, to enable consistent processing (and reporting) throughout.
