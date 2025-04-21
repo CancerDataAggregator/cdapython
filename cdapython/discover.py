805f08eeaa81c56f66ec0d0ed22ef0d8c1f8aca8
abradyGDIT <arthur.brady@gdit.com>
2025-04-21 10:20:10 -0400

    Adding cda_functions() function

--- cdapython/discover.py
+++ cdapython/discover.py
@@ -35,7 +35,7 @@ def cda_functions():
 
     log = get_logger()
 
-    log.info( 'You can get complete usage details for each function by calling "help( function_name )".' )
+    log.error( 'You can get complete usage details for each function by calling "help( function_name )".' )
 
     return sorted(
         {
