e3cba6fac246406e88562c4e3b41206a67046640
abradyGDIT <arthur.brady@gdit.com>
2025-05-06 15:31:31 -0400

    Adding new handlers for provenance parameter

--- cdapython/get_data.py
+++ cdapython/get_data.py
@@ -997,7 +997,10 @@ def get_data(
                         
                         result_dataframe[column] = pd.to_numeric( result_dataframe[column] ).round().astype( 'Int64' )
 
-                    result_dataframe[column] = result_dataframe[column].apply( lambda cell_val: [ numpy.int64( round( element_val ) ) if element_val == element_val else '<NA>' for element_val in cell_val ] if isinstance( cell_val, list ) else numpy.int64( round( cell_val ) ) if cell_val == cell_val else '<NA>' )
+                    # (float) NaN != NaN
+                    # Testing cell values for None will miss NaN values, which will then generate an error if uncaught before trying to round them.
+
+                    result_dataframe[column] = result_dataframe[column].apply( lambda cell_val: [ numpy.int64( round( element_val ) ) if ( element_val is not None and element_val == element_val ) else '<NA>' for element_val in cell_val ] if isinstance( cell_val, list ) else numpy.int64( round( cell_val ) ) if ( cell_val is not None and cell_val == cell_val ) else '<NA>' )
 
                     # result_dataframe[column] = result_dataframe[column].apply( lambda cell_val: [ numpy.int64( round( element_val ) ) if element_val is not None else '<NA>' for element_val in cell_val ] if isinstance( cell_val, list ) else numpy.int64( round( cell_val ) ) if cell_val is not None else '<NA>' )
 
