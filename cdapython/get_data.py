89b6e1753956ceffb64dea43c13ed4797794e95e
abradyGDIT <arthur.brady@gdit.com>
2025-05-06 15:24:05 -0400

    Adding new handlers for provenance parameter

--- cdapython/get_data.py
+++ cdapython/get_data.py
@@ -1047,7 +1047,7 @@ def get_data(
                             
                             row_data.append( result_record[column].to_dict( orient='records' ) )
 
-                        elif result_record[column] == '<NA>':
+                        elif isinstance( result_record[column], str ) and result_record[column] == '<NA>':
                             
                             row_data.append( '' )
 
