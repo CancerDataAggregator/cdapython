6623632133908fe9046e86c320d89150ade071c4
abradyGDIT <arthur.brady@gdit.com>
2025-05-06 15:29:29 -0400

    Adding new handlers for provenance parameter

--- cdapython/get_data.py
+++ cdapython/get_data.py
@@ -997,7 +997,9 @@ def get_data(
                         
                         result_dataframe[column] = pd.to_numeric( result_dataframe[column] ).round().astype( 'Int64' )
 
-                    result_dataframe[column] = result_dataframe[column].apply( lambda cell_val: [ numpy.int64( round( element_val ) ) if element_val is not None else '<NA>' for element_val in cell_val ] if isinstance( cell_val, list ) else numpy.int64( round( cell_val ) ) if cell_val is not None else '<NA>' )
+                    result_dataframe[column] = result_dataframe[column].apply( lambda cell_val: [ numpy.int64( round( element_val ) ) if element_val == element_val else '<NA>' for element_val in cell_val ] if isinstance( cell_val, list ) else numpy.int64( round( cell_val ) ) if cell_val == cell_val else '<NA>' )
+
+                    # result_dataframe[column] = result_dataframe[column].apply( lambda cell_val: [ numpy.int64( round( element_val ) ) if element_val is not None else '<NA>' for element_val in cell_val ] if isinstance( cell_val, list ) else numpy.int64( round( cell_val ) ) if cell_val is not None else '<NA>' )
 
                 elif column_data_types[column] in { 'text', 'boolean' }:
                     
