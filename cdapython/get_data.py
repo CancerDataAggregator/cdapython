c04311447adb10530137ffc79051bc41fb851865
abradyGDIT <arthur.brady@gdit.com>
2025-05-06 14:14:21 -0400

    Adding new handlers for provenance parameter

--- cdapython/get_data.py
+++ cdapython/get_data.py
@@ -908,7 +908,7 @@ def get_data(
 
         for row_index, result_record in result_dataframe.iterrows():
             for identifier_record in result_record[ 'upstream_identifiers_columns' ]:
-                result_dataframe['provenance'].iloc[row_index] = pd.concat( [ result_dataframe['provenance'].iloc[row_index], pd.DataFrame.from_dict( identifier_record ) ] )
+                result_dataframe['provenance'].iloc[row_index] = pd.concat( [ result_dataframe['provenance'].iloc[row_index], pd.DataFrame.from_dict( identifier_record ) ], ignore_index=True )
 
     return result_dataframe
 
