d144448dbd278ac33c9b87dbfa49a8203fbd81fd
abradyGDIT <arthur.brady@gdit.com>
2025-05-10 12:14:48 -0400

    Adding virtual subject column to support upstream ID search

--- cdapython/get_data.py
+++ cdapython/get_data.py
@@ -5,7 +5,7 @@ import re
 
 import cda_client
 
-from cdapython.application_utilities import build_match_from_file_filter, get_api_url
+from cdapython.application_utilities import get_api_url
 from cdapython.discover import columns, release_metadata
 from cdapython.logging_wrappers import get_logger
 from cdapython.validation import normalize_to_list, validate_and_transform_match_filter_list, validate_parameter_values
