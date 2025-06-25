7fce4348b4f4586200dca735cfff7907653d257e
abradyGDIT <arthur.brady@gdit.com>
2025-06-25 13:40:16 -0400

    Hardening summary requests in presence of handleable errors

--- cdapython/summarize.py
+++ cdapython/summarize.py
@@ -744,7 +744,7 @@ def summarize(
             body=query_object
         )
     except UnexpectedStatus as error:
-        log.error( f"UnexpectedStatus error from API, status code {error.status_code}: {error.messsage}" )
+        log.error( f"UnexpectedStatus error from API, status code {error.status_code}: {json.loads( error.content )['message']}" )
         return
     except Exception as error:
         log.error( f"{type(error)}: {error}" )
