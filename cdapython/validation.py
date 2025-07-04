d5b8e4ce4a268e67056a729d9cc47d119d61fc60
abradyGDIT <arthur.brady@gdit.com>
2025-07-04 14:39:48 -0400

    Smoke testing new integrations

--- cdapython/validation.py
+++ cdapython/validation.py
@@ -140,7 +140,7 @@ def validate_and_transform_match_filter_list( cached_column_metadata, match_stat
 
     for filter_expression in match_statement_list:
         
-        print( 'ding', file=sys.stderr )
+        print( f'ding: {filter_expression}', file=sys.stderr )
         #############################################################################################################################
         # Enforce the simplified cdapython query syntax as described in the docs, but quietly allow synonyms if received. Map them
         # first back to the canonical operator in the simplified query syntax, then validate, then normalize for API request syntax
