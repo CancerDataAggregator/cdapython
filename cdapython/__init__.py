e2f614c0d5dd45536299ffaee7c64080b5f112b1
abradyGDIT <arthur.brady@gdit.com>
2025-07-31 12:00:16 -0400

    Adding expand_file_results() and expand_subject_results()

--- cdapython/__init__.py
+++ cdapython/__init__.py
@@ -1,6 +1,6 @@
 from cdapython.application_utilities import get_api_url, set_api_url
 from cdapython.discover import cda_functions, column_values, columns, release_metadata, tables
-from cdapython.summarize import intersect_file_results, intersect_subject_results, summarize_files, summarize_subjects
+from cdapython.summarize import expand_file_results, expand_subject_results, intersect_file_results, intersect_subject_results, summarize_files, summarize_subjects
 from cdapython.get_data import get_file_data, get_subject_data
 
 from cdapython.logging_wrappers import disable_console_logging, disable_file_logging, enable_console_logging, enable_file_logging, get_log_level, get_valid_log_levels, set_log_level
