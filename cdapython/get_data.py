ef62fb147f4a9dc7e9071590aab8a770c0def51d
abradyGDIT <arthur.brady@gdit.com>
2025-12-09 12:48:13 -0500

    Adding POC slim functionality to get_data()

--- cdapython/get_data.py
+++ cdapython/get_data.py
@@ -1477,6 +1477,7 @@ def get_data(
     if include_disease_slims and not collate_results and ( 'diagnosis' in result_dataframe or 'morphology' in result_dataframe ):
         # Make a new column called 'disease_slims', populated with slim terms corresponding to any mapped terms from `observation.diagnosis` or `observation.morphology`.
         result_dataframe['disease_slims'] = [ set() for _ in range( len( result_dataframe ) ) ]
+        column_data_types['disease_slims'] = 'text'
         for row_index, result_record in result_dataframe.iterrows():
             for disease_column in [ 'diagnosis', 'morphology' ]:
                 if disease_column in result_dataframe:
