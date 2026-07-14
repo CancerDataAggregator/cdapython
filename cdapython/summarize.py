ee035a155e53a6a6f56cf66a0377a1fcf9e08c35
abradyGDIT <arthur.brady@gdit.com>
2026-07-14 14:27:56 -0400

    Adjusting context-adaptive table formatting to only separate rows in the presence of multiline cells

--- cdapython/summarize.py
+++ cdapython/summarize.py
@@ -2338,7 +2338,7 @@ def summarize(
                             print_df[print_df.columns[0]] = print_df[print_df.columns[0]].apply( lambda x: re.sub( f"^(.{{{max_col_width-3}}}).*", r"\1...", x ) if ( x is not None and not isinstance( x, bool ) and len( x ) > max_col_width ) else x )
                             for extra_list_type in extra_list_types:
                                 if extra_list_type in print_df.columns.values:
-                                    print_df[print_df.columns[print_df.columns.get_loc( extra_list_type )]] = print_df[print_df.columns[print_df.columns.get_loc( extra_list_type )]].apply( lambda x: re.sub( f"^(.{{{max_col_width-3}}}).*", r"\1...", x, flags=re.MULTILINE ) if ( x is not None and not isinstance( x, bool ) and len( x ) > max_col_width ) else x ).apply( lambda y: re.sub( r"(^(?:.*\n){3})(.*\n)*.*", r"\1\n...", y, flags=re.MULTILINE ) )
+                                    print_df[print_df.columns[print_df.columns.get_loc( extra_list_type )]] = print_df[print_df.columns[print_df.columns.get_loc( extra_list_type )]].apply( lambda x: re.sub( f"^(.{{{max_col_width-3}}}).*", r"\1...", x, flags=re.MULTILINE ) if ( x is not None and not isinstance( x, bool ) and len( x ) > max_col_width ) else x ).apply( lambda y: re.sub( r"(^(?:.*\n){3})(.*\n)*.*", r"\1...", y, flags=re.MULTILINE ) )
 
                             # Put the count values first in the display.
                             new_column_ordering = []
