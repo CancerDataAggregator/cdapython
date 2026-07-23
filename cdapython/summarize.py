c47555947c875291d97ef5d17665dd86d858713e
abradyGDIT <arthur.brady@gdit.com>
2026-07-23 11:34:49 -0400

    Updating synonym display lists to filter (until more context is delivered) same-name terms from different vocabularies

--- cdapython/summarize.py
+++ cdapython/summarize.py
@@ -2544,7 +2544,7 @@ def summarize(
                                 # identifiers or other clarifying context. Remove redundant entries.
                                 # 
                                 # Note this filter is performed in two places in this script.
-                                if 'synonym_terms' in result_column_dict and f"{result_column}_synonym_terms" in dict_record and len( dict_record[f"{result_column}_synonym_terms"] ) > 0:
+                                if f"{result_column}_synonym_terms" in dict_record and len( dict_record[f"{result_column}_synonym_terms"] ) > 0:
                                     dict_record[f"{result_column}_synonym_terms"] = [ synonym_term for synonym_term in dict_record[f"{result_column}_synonym_terms"] if synonym_term.lower() != print_value.lower() ]
 
                                 for extra_list_type in extra_list_types:
