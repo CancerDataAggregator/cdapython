34a03a6478efc7a67c6ea4965def8caa6b5656e3
abradyGDIT <arthur.brady@gdit.com>
2026-07-27 10:55:57 -0400

    Alter return flows to evade disallowed null-value keys for JSON output option

--- cdapython/summarize.py
+++ cdapython/summarize.py
@@ -2483,9 +2483,9 @@ def summarize(
                 # JSON won't allow null keys. Change None to '' before dumping.
                 result_column_list = result_dict.keys()
                 for result_column in result_column_list:
-                    if None in result_dict[result_column]:
-                        print( f'DARN! {result_column}' )
-                        #result_dict[result_column][''] = result_dict[result_column].pop( None )
+                    if isinstance( result_dict[result_column], dict ) and None in result_dict[result_column]:
+                        #print( f'DARN! {result_column}' )
+                        result_dict[result_column][''] = result_dict[result_column].pop( None )
                 with open( output_file, 'w' ) as OUT:
                     json.dump( result_dict, OUT, indent=4, ensure_ascii=True )
                 return
